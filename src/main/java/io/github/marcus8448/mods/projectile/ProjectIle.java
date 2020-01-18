2cf899f4a7199fc3e2c7d86a0723defb1c5b2baa
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2020-01-18 13:54:33 -0800

    1.15 mappings

--- src/main/java/io/github/marcus8448/mods/projectile/ProjectIle.java
+++ src/main/java/io/github/marcus8448/mods/projectile/ProjectIle.java
@@ -17,7 +17,6 @@
 
 package io.github.marcus8448.mods.projectile;
 
-import io.github.marcus8448.mods.projectile.client.render.entity.WolfRendererBall;
 import io.github.marcus8448.mods.projectile.entity.ai.goal.FollowBallGoal;
 import io.github.marcus8448.mods.projectile.entity.projectile.BouncyBallEntity;
 import io.github.marcus8448.mods.projectile.entity.projectile.BouncyDynamiteEntity;
@@ -26,8 +25,6 @@ import io.github.marcus8448.mods.projectile.entity.projectile.DynamiteEntity;
 import io.github.marcus8448.mods.projectile.item.*;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.SpriteRenderer;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
@@ -43,12 +40,10 @@ import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
